8885c24c21119c284331ec5cf1d55a9dfdc7a976
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-04-05 13:40:12 +0900

    USE UT

--- src/Components/Propertylist/index.jsx
+++ src/Components/Propertylist/index.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const { REACT_APP_BASE_URL: url } = process.env;
 export const Propertylist = () => {
+  // console.log(process.env);
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    fetch(`${url}/v1//houses`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res?.data);
+      });
+  }, []);
   return (
     <div>
       <h1>Propertylist</h1>
@@ -8,3 +24,5 @@ export const Propertylist = () => {
 };
 
 export default Propertylist;
+
+// body malumot jonatganda back end ga 'Content Type':'application/json'
