aebe0af9b2f10ed030f9c074d7ec04fd3d710840
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-04-06 01:15:56 +0900

    use that

--- src/Components/Addproperty/index.jsx
+++ src/Components/Addproperty/index.jsx
@@ -1,5 +1,46 @@
 import React from "react";
-import { Adding_Wrapper, Add_Word, Container, New_Word } from "./style";
+import {
+  Adding_Wrapper,
+  Additional_Div,
+  Add_Word,
+  All_Wrapper,
+  Area,
+  Bath,
+  Beds,
+  Built,
+  Contact_Div,
+  Container,
+  Description,
+  Dimension,
+  First_Div,
+  First_Input,
+  Garage,
+  Information,
+  Label,
+  L_area,
+  Material,
+  New_Word,
+  Prefix,
+  Price_Div,
+  Price_Word,
+  PropId,
+  Room,
+  Second_Div,
+  Status,
+  Third_Div,
+  Title,
+  Twelve,
+  Type,
+  Type_Wrapper,
+  Under,
+  Word_Addition,
+  Second_Input,
+  Suffix,
+  Third_Input,
+  Fourth_Input,
+  Location_Div,
+  Word_Loc,
+} from "./style";
 export const Addproperty = () => {
   return (
     <Container>
@@ -7,6 +48,53 @@ export const Addproperty = () => {
         <Add_Word>Add New Property</Add_Word>
         <New_Word>Ready to jump back in?</New_Word>
       </Adding_Wrapper>
+      <Contact_Div>
+        <Information>Contact Information</Information>
+        <Type_Wrapper>
+          <Title type="text" placeholder="Property Title" />
+          <Type type="text" placeholder="Type" />
+        </Type_Wrapper>
+        <Description>Property Description</Description>
+        <Under type="text" />
+      </Contact_Div>
+      <Additional_Div>
+        <Word_Addition>Additional</Word_Addition>
+        <All_Wrapper>
+          <First_Div>
+            <PropId type="text" placeholder="Property ID" />
+            <Label type="text" placeholder="Label" />
+            <Beds type="text" placeholder="Beds" />
+            <Built type="text" placeholder="Year Built" />
+            <L_area type="text" placeholder="Lot Area (sqft)" />
+          </First_Div>
+          <Second_Div>
+            <Twelve type="text" placeholder="12" />
+            <Material type="text" placeholder="Material" />
+            <Bath type="text" placeholder="Bath" />
+            <Area type="text" placeholder="Home Area (sqft)" />
+          </Second_Div>
+          <Third_Div>
+            <Status type="text" placeholder="Status" />
+            <Room type="text" placeholder="Rooms" />
+            <Garage type="text" placeholder="Garages" />
+            <Dimension type="text" placeholder="Lot Dimensions" />
+          </Third_Div>
+        </All_Wrapper>
+      </Additional_Div>
+      <Price_Div>
+        <Price_Word>Price</Price_Word>
+        <Prefix>
+          <First_Input type="text" placeholder="Price(S)" />
+          <Second_Input type="text" placeholder="Price Prefix" />
+        </Prefix>
+        <Suffix>
+          <Third_Input type="text" placeholder="Price Suffix" />
+          <Fourth_Input type="text" placeholder="Price Custom" />
+        </Suffix>
+      </Price_Div>
+      <Location_Div>
+        <Word_Loc>Location</Word_Loc>
+      </Location_Div>
     </Container>
   );
 };
--- src/Components/Addproperty/style.js
+++ src/Components/Addproperty/style.js
@@ -3,8 +3,10 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 100%;
   border: 1px solid black;
-  height: 930px;
+  height: 1930px;
   background: #e5e5e5;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Adding_Wrapper = styled.div`
@@ -33,4 +35,333 @@ const New_Word = styled.p`
   color: #696969;
   margin-top: -30px;
 `;
-export { Container, Add_Word, Adding_Wrapper, New_Word };
+const Contact_Div = styled.div`
+  margin-left: 50px;
+  height: 300px;
+  width: 1160px;
+  margin-top: 60px;
+  background: #ffffff;
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+`;
+const Information = styled.p`
+  font-family: "Cerebri Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  color: #0d263b;
+  margin-left: 30px;
+`;
+const Type_Wrapper = styled.div`
+  display: flex;
+  margin-left: 30px;
+  justify-content: space-between;
+  margin-top: 45px;
+`;
+const Title = styled.input`
+  width: 530px;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Type = styled.input`
+  width: 530px;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #e6e9ec;
+  margin-right: 30px;
+`;
+const Description = styled.p`
+  font-family: "Cerebri Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  /* identical to box height */
+
+  /* Text / Color */
+
+  color: #696969;
+  margin-left: 30px;
+  margin-top: 40px;
+`;
+const Under = styled.input`
+  width: 1100px;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #e6e9ec;
+  margin-left: 30px;
+  margin-top: 60px;
+`;
+const Additional_Div = styled.div`
+  margin-left: 50px;
+  height: 300px;
+  width: 1160px;
+  margin-top: 30px;
+  background: #ffffff;
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+`;
+const Word_Addition = styled.p`
+  font-family: "Cerebri Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  margin-left: 30px;
+  color: #0d263b;
+`;
+const All_Wrapper = styled.div`
+  display: flex;
+`;
+const First_Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const PropId = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 15px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Label = styled.input`
+  border: none;
+  width: 340px;
+  margin-top: 20px;
+  outline: none;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Beds = styled.input`
+  border: none;
+  width: 340px;
+  margin-top: 20px;
+  outline: none;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Built = styled.input`
+  border: none;
+  width: 340px;
+  margin-top: 20px;
+  outline: none;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const L_area = styled.input`
+  border: none;
+  width: 340px;
+  margin-top: 20px;
+  outline: none;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+
+const Second_Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Twelve = styled.input`
+  border: none;
+  outline: none;
+  margin-left: 30px;
+  width: 340px;
+  margin-top: 20px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Material = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Bath = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Area = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Third_Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Status = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Room = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Garage = styled.input`
+  border: none;
+  outline: none;
+  width: 340px;
+  margin-top: 20px;
+  margin-left: 30px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Dimension = styled.input`
+  border: none;
+  outline: none;
+  margin-left: 30px;
+  width: 340px;
+  margin-top: 20px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Price_Div = styled.div`
+  margin-left: 50px;
+  height: 200px;
+  width: 1160px;
+  margin-top: 30px;
+  background: #ffffff;
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+`;
+const Price_Word = styled.p`
+  font-family: "Cerebri Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  color: #0d263b;
+  margin-left: 30px;
+`;
+const Prefix = styled.div`
+  justify-content: space-between;
+  display: flex;
+  margin-left: 30px;
+`;
+const First_Input = styled.input`
+  border: none;
+  outline: none;
+  width: 520px;
+  margin-top: 20px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Second_Input = styled.input`
+  border: none;
+  outline: none;
+  width: 520px;
+  margin-top: 20px;
+  margin-right: 35px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Suffix = styled.div`
+  justify-content: space-between;
+  display: flex;
+  margin-left: 30px;
+  margin-top: 30px;
+`;
+const Third_Input = styled.input`
+  border: none;
+  outline: none;
+  width: 520px;
+  margin-top: 20px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Fourth_Input = styled.input`
+  border: none;
+  outline: none;
+  width: 520px;
+  margin-top: 20px;
+  margin-right: 35px;
+  border-bottom: 1px solid #e6e9ec;
+`;
+const Location_Div = styled.div`
+  margin-left: 50px;
+  height: 600px;
+  width: 1160px;
+  margin-top: 30px;
+  background: #ffffff;
+  border: 1px solid #e6e9ec;
+  box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+`;
+const Word_Loc = styled.p`
+  font-family: "Cerebri Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+
+  /* Color / 2 */
+
+  color: #0d263b;
+  margin-left: 30px;
+`;
+
+export {
+  Word_Loc,
+  Location_Div,
+  Third_Input,
+  Fourth_Input,
+  Prefix,
+  Suffix,
+  First_Input,
+  Second_Input,
+  Price_Word,
+  Price_Div,
+  L_area,
+  All_Wrapper,
+  Third_Div,
+  Status,
+  Room,
+  Garage,
+  Dimension,
+  Second_Div,
+  Twelve,
+  Material,
+  Bath,
+  Area,
+  First_Div,
+  PropId,
+  Label,
+  Beds,
+  Built,
+  Word_Addition,
+  Additional_Div,
+  Under,
+  Description,
+  Type_Wrapper,
+  Title,
+  Type,
+  Information,
+  Contact_Div,
+  Container,
+  Add_Word,
+  Adding_Wrapper,
+  New_Word,
+};
